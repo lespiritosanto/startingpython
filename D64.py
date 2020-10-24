1df2fd6c8d6355a12afc3ff0262bce729f5fd6b7
Halzera <espiritosanto.engenharia@gmail.com>
2020-10-24 17:10:49 -0300

    wtf

--- D64.py
+++ D64.py
@@ -1,10 +1,24 @@
-# create a program that reads integer numbers. It stops when the user types
-# 999. It should count the numbers typed and their sum, excluding the
-# stop value/ flag
+[33mcommit e9996d7a673ced5f3c66c62445764c1ea4321e99[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m)[m
+Merge: 909b703 139fdc5
+Author: Halzera <espiritosanto.engenharia@gmail.com>
+Date:   Sat Oct 24 17:07:59 2020 -0300
 
-<<<<<<< HEAD
-print('Hello There!!')
-=======
-print('I am learning how to use git')
->>>>>>> test
+    last change to test
 
+[33mcommit 139fdc5a473fb9318c5d6891c9fc1d69f58bfdae[m[33m ([m[1;32mtest[m[33m)[m
+Author: Halzera <espiritosanto.engenharia@gmail.com>
+Date:   Sat Oct 24 17:02:37 2020 -0300
+
+    testing item on branch
+
+[33mcommit 909b7035d03ece785c591ca4d8137aad05b7b5b4[m
+Author: Halzera <espiritosanto.engenharia@gmail.com>
+Date:   Sat Oct 24 16:47:32 2020 -0300
+
+    putting basic code
+
+[33mcommit 07c0383ef382be8a8275c900c56d78a8d8bd93a9[m
+Author: Halzera <espiritosanto.engenharia@gmail.com>
+Date:   Sat Oct 24 16:45:01 2020 -0300
+
+    testing git in py
