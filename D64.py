6895165f7a42c4f97c7de86fa469123386c9ef3e
Halzera <espiritosanto.engenharia@gmail.com>
2020-10-24 17:17:51 -0300

    testing git

--- D64.py
+++ D64.py
@@ -1,24 +1,20 @@
-[33mcommit e9996d7a673ced5f3c66c62445764c1ea4321e99[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m)[m
-Merge: 909b703 139fdc5
-Author: Halzera <espiritosanto.engenharia@gmail.com>
-Date:   Sat Oct 24 17:07:59 2020 -0300
+# recreate exercise 61, asking the user if he wants
+# the program to calculate more terms. The program should
+# end if the user writes 0
 
-    last change to test
+ratio = float(input('Type the ratio of the AP: '))
+term = float(input('Type the first term of the AP: '))
+counter = 1
+total = 0
+more = 10
 
-[33mcommit 139fdc5a473fb9318c5d6891c9fc1d69f58bfdae[m[33m ([m[1;32mtest[m[33m)[m
-Author: Halzera <espiritosanto.engenharia@gmail.com>
-Date:   Sat Oct 24 17:02:37 2020 -0300
+while more != 0:
+    total = total + more
+    while counter <= total:
+        print('The {}° term of the AP is: {}'.format(counter, term + ratio * (counter - 1)))
+        counter += 1
 
-    testing item on branch
+    more = int(input('How many terms you want more? '))
 
-[33mcommit 909b7035d03ece785c591ca4d8137aad05b7b5b4[m
-Author: Halzera <espiritosanto.engenharia@gmail.com>
-Date:   Sat Oct 24 16:47:32 2020 -0300
 
-    putting basic code
 
-[33mcommit 07c0383ef382be8a8275c900c56d78a8d8bd93a9[m
-Author: Halzera <espiritosanto.engenharia@gmail.com>
-Date:   Sat Oct 24 16:45:01 2020 -0300
-
-    testing git in py
