6f9f7263fd945f81b46f40deb9167c44fc0ea773
Halzera <espiritosanto.engenharia@gmail.com>
2020-10-27 14:20:46 -0300

    finished D69

--- D69.py
+++ D69.py
@@ -3,4 +3,27 @@
 # at the end it should show how many people have more than 18, how many men
 # and how many women have less than 20
 
+sex_count = man_age_count = women_age_count = age_count = 0
+choice = ''
 
+while True:
+    print('+=' * 25)
+    age = int(input('Type your age: '))
+    if age > 18:
+        age_count += 1
+
+    sex = str(input('Type your gender [m/f]: ')).strip().casefold()[0]
+    while sex[0] not in "mf":
+        sex = str(input('Type your gender [m/f]: ')).strip().casefold()[0]
+
+    if sex == 'm' and age < 18:
+        man_age_count += 1
+
+    if sex == 'f' and age < 18:
+        women_age_count += 1
+
+    choice = input('Do you want to continue with the register [y/n]? ').strip().casefold()[0]
+    if choice[0] == 'n':
+        break
+print('There are {} adults, {} men and {} women have less than 18.'
+      .format(age_count, man_age_count, women_age_count))
