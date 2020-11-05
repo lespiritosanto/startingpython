5e9c4f795c5cc6025cf7c447a1bfde2c12c5407a
Halzera <espiritosanto.engenharia@gmail.com>
2020-11-05 01:30:03 -0300

    'done'

--- D84.py
+++ D84.py
@@ -2,28 +2,43 @@
 # at the end, show how many ppl were registered; a list of the heaviest and another
 # of the lightest
 
-people = []
-choice = ' '
+ppl = []
+name_wei = []
 
-while choice != 'n':
-    people.append(str(input('Type your name: ').strip().capitalize()))
-    people.append(int(input('Type your weight: ')))
-    print('+=' * 25)
+while True:
+    name_wei.append(str(input('Type the name: ').strip().capitalize()))
+    name_wei.append(float(input('Type the weight: ')))
 
-    choice = str(input('Do you want to continue? [y/n] ')).strip().casefold()
+    ppl.append(name_wei[:])
+    name_wei.clear()
 
-print('The number of people registered were: {}.'.format(len(people) // 2))
+    choice = input('Do you want to continue? [y/n] ').strip().casefold()
+    print('=+' * 30)
+    if choice == 'n':
+        break
 
-maximum = 0
-for char in people:
-    if type(char) == int:
-        if people[1] == char:
-            lightest = char
-        if char > maximum:
-            maximum = char
-        if char < lightest:
-            lightest = char
+print('There were {} registered people.'.format(len(ppl)))
 
-print('The heaviest person is: {}'.format(people[people.index(maximum) - 1]))
+big = small = ppl[0][1]
+for person in ppl:
+    for weight in person:
+        if type(weight) == float:
+            if weight > big:
+                big = weight
+            elif weight < small:
+                small = weight
 
-print('The lightest person is: {}'.format(people[people.index(lightest) - 1]))
+print('The heaviest person is {:.2f} kg.'.format(big))
+print('The lightest person is {:.2f} Kg.'.format(small))
+print('=+' * 30)
+
+print('The heaviest are: ', end='')
+for person in ppl:
+    if person[1] == big:
+        print('{}'.format(person[0]), end=' ')
+
+print()
+print('The lightest are: ', end='')
+for person in ppl:
+    if person[1] == small:
+        print('{}'.format(person[0]), end=' ')
